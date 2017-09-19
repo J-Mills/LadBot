b840bce88a82e3bc1f67361ac7f8a74088644d91
Jack Mills <jack.mills@LM-JAM-02.local>
2017-09-19 12:40:31 +0100

    Make the buy command smarter

--- commands/buy.js
+++ commands/buy.js
@@ -2,14 +2,22 @@ const shop = require('../shop.json');
 
 exports.run = async (client, message, args) => {
   let userData = points[message.author.id];
+  let item = args[0];
+  let amount = args[1];
 
-  // if (args[0] == )
-
-  if (args[0] == 'booster') {
-    if (userData.coins > shop.booster.price) {
-      userData.items.booster++;
-      userData.coins -= shop.booster.price;
-      message.channel.send('Booster bought!');
+  if (args[1]) {
+    if ((userData.coins) > (shop[item].price * amount)) {
+      userData.items[item] += parseInt(amount);
+      userData.coins -= (shop[item].price * amount);
+      message.channel.send(`${amount} ${item}'s bought!`);
+    } else {
+      message.channel.send(`You don't have enough coins!`);
+    }
+  } else {
+    if (userData.coins > shop[item].price) {
+      userData.items[item]++;
+      userData.coins -= shop[item].price;
+      message.channel.send(`${item} bought!`);
     } else {
       message.channel.send(`You don't have enough coins!`);
     }
